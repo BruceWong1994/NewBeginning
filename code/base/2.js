f282140a869d46ceb3dfab60766b1e40cffde538
767961652@qq.com <767961652@qq.com>
2024-03-23 22:53:23 +0800

    复习到dom操作前一节

---
+++ code/base/2.js
@@ -0,0 +1,27 @@
+"use strict";
+var a;
+console.log(a);
+a = 20/6;
+console.log(a);
+// console.log
+a = a.toFixed(2);
+console.log(a);
+
+
+function tt(){
+    a++;
+    console.log(a);
+    return '11111';
+}
+var b = tt();
+console.log(b);
+
+var c = new Array();
+console.log(c);
+c=[1,5,66,82,12,4];
+console.log(c);
+console.log(c[c.length-1]);
+
+for(var i= 0;i<c.length;i++){
+    console.log(c[i]);
+}
