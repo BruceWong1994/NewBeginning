a9342211259c3cccc8fbc3cbd5dac8a401c0fae0
767961652@qq.com <767961652@qq.com>
2024-03-25 17:25:31 +0800

    d3学习js核心内容

---
+++ code/base/3.js
@@ -0,0 +1,13 @@
+"use strict";
+var d3 = document.getElementById("day3");
+console.log(d3);
+
+function dd3(){
+    d3.innerHTML='变了吧';
+    console.log(this);
+}
+
+function cc(){
+    console.log('进来了？');
+    
+}
